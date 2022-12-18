9ff13ca13fbcd7becad3e171a894245182fdfbe6
Kjell May <kjell.may@haw-hamburg.de>
2022-12-18 21:11:01 +0100

    implemented changePlayerDirection, getPlayerById, calcNextPos; added extra param for calcNextPos in ADs

--- src/main/java/de/hawh/beta3/application/game/model/gamelogic/GameLogic.java
+++ src/main/java/de/hawh/beta3/application/game/model/gamelogic/GameLogic.java
@@ -24,14 +24,14 @@ public class GameLogic implements IGameLogic {
         Direction[] startingDir;
         if (numPlayers > 4) {
             startingPos = new Position[]{
-                    new Position(0, (size-1)/3),
-                    new Position((size-1), (size-1)/3),
-                    new Position((size-1)/2, 0),
-                    new Position((size-1)/2, (size-1)),
-                    new Position(0, 2*(size-1)/3),
-                    new Position((size-1), 2*(size-1)/3)
+                    new Position(0, (size - 1) / 3),
+                    new Position((size - 1), (size - 1) / 3),
+                    new Position((size - 1) / 2, 0),
+                    new Position((size - 1) / 2, (size - 1)),
+                    new Position(0, 2 * (size - 1) / 3),
+                    new Position((size - 1), 2 * (size - 1) / 3)
             };
-            startingDir = new Direction[] {
+            startingDir = new Direction[]{
                     Direction.RIGHT,
                     Direction.LEFT,
                     Direction.DOWN,
@@ -41,12 +41,12 @@ public class GameLogic implements IGameLogic {
             };
         } else {
             startingPos = new Position[]{
-                    new Position(0, (size-1)/2),
-                    new Position((size-1), (size-1)/2),
-                    new Position((size-1)/2, 0),
-                    new Position((size-1)/2, (size-1))
+                    new Position(0, (size - 1) / 2),
+                    new Position((size - 1), (size - 1) / 2),
+                    new Position((size - 1) / 2, 0),
+                    new Position((size - 1) / 2, (size - 1))
             };
-            startingDir = new Direction[] {
+            startingDir = new Direction[]{
                     Direction.RIGHT,
                     Direction.LEFT,
                     Direction.DOWN,
@@ -67,7 +67,15 @@ public class GameLogic implements IGameLogic {
      */
     @Override
     public void changePlayerDirection(int id, String action) {
+        //if (id < 0 || id > players.size()) return; //needed?
+        Player p = getPlayerById(id);
 
+        if (p.isAlive() && p.getCurrentAction() == null) {
+            Position pos = calcNextPos(p.getFront(), p.getDirection(), action);
+            p.setFront(pos);
+            p.setDirection(action.equals("left") ? p.getDirection().prev() : p.getDirection().next());
+            p.setCurrentAction(action);
+        }
     }
 
     /**
@@ -103,12 +111,40 @@ public class GameLogic implements IGameLogic {
         return players;
     }
 
+    /**
+     * Method is optimistic id is correct. It's called <code>getPlayerById</code> and not <code>findPlayerById</code>
+     *
+     * @param id id of player to find
+     * @return player with id
+     */
     private Player getPlayerById(int id) {
-        return null;
+        return players.stream().filter(p -> p.getColor() == id).findFirst().get();
     }
 
-    private Position calcNextPos(Position position, Direction direction) {
-        return null;
+    /**
+     * Calculates the next position given by current <code>position</code> and turn in <code>direction</code>
+     *
+     * @param position   current position
+     * @param pDirection direction of player
+     * @param action     "left", "right" or null
+     * @return new position
+     */
+    private Position calcNextPos(Position position, Direction pDirection, String action) {
+        int[] xyPositionChange = new int[]{-1, 0, 0, -1, 1, 0, 0, 1};
+        Direction resDirection;
+
+        if (action.equals("left")) {
+            resDirection = pDirection.prev();
+        } else if (action.equals("right")) {
+            resDirection = pDirection.next();
+        } else {
+            resDirection = pDirection;
+        }
+
+        int x = position.getX() + xyPositionChange[2 * resDirection.ordinal()];
+        int y = position.getY() + xyPositionChange[2 * resDirection.ordinal() + 1];
+
+        return new Position(x, y);
     }
 
     private void movePlayer(Player p) {
@@ -128,7 +164,7 @@ public class GameLogic implements IGameLogic {
         return false;
     }
 
-    private void killPlayers (List<Player> players) {
+    private void killPlayers(List<Player> players) {
 
     }
 }
