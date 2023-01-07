60da4417a0b0e7755bf61c9fa19a6c98ace8be4e
viviam_ribeiro <Viviam.Ribeiro@haw-hamburg.de>
2023-01-07 06:16:31 +0100

    JavaFX-Unterstüzung in pom-Datei hinzugefügt und module-info mit JavaFX-Requirements erstellt

---
+++ src/main/java/de/hawh/beta3/application/game/view/HelloApplication.java
@@ -0,0 +1,23 @@
+package de.hawh.beta3.application.game.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        stage.setTitle("Hello!");
+
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
