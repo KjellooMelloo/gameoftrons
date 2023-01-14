c60a17afda99ce32f3ee1e1e1eedd84a5d3cebe4
Kathleen Neitzel <kathleen.neitzel@haw-hamburg.de>
2023-01-14 21:40:27 +0100

    Factory liefert Singletons für MVC mit GameManager, ModelViewImpl und Context. Middleware über AppStubCaller als Singleton instanziiert. Factory für ControllerViewImpl?

--- src/main/java/de/hawh/beta3/application/factory/IModelViewFactory.java
+++ src/main/java/de/hawh/beta3/application/factory/IModelViewFactory.java
@@ -1,11 +1,12 @@
 package de.hawh.beta3.application.factory;
 
 import de.hawh.beta3.application.game.view.IModelView;
+import de.hawh.beta3.application.game.view.Screen.IModelViewImpl;
 import de.hawh.beta3.application.stub.caller.IModelViewCaller;
 
 class IModelViewFactory {
 
     public static IModelView getModelView(boolean remote) {
-        return remote ? new IModelViewCaller()  /*IModelViewCaller.getInstance()*/ : null; //ModelViewImpl.getInstance();
+        return remote ? new IModelViewCaller()  /*IModelViewCaller.getInstance()*/ : IModelViewImpl.getInstance(); //;
     }
 }
