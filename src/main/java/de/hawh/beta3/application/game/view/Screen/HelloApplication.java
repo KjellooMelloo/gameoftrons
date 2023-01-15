e8e843835cf3750197f196fcb02f39c76848a8e6
viviam_ribeiro <Viviam.Ribeiro@haw-hamburg.de>
2023-01-15 02:20:10 +0100

    All ready for controller calls implementation

--- src/main/java/de/hawh/beta3/application/game/view/Screen/HelloApplication.java
+++ src/main/java/de/hawh/beta3/application/game/view/Screen/HelloApplication.java
@@ -14,10 +14,7 @@ public class HelloApplication extends Application {
         stage.setHeight(javafx.stage.Screen.getPrimary().getBounds().getHeight() / 1.5);
 
         ScreenManager gameScreen = new ScreenManager();
-        gameScreen.drawScreen("end");
-        EndScreen es = (EndScreen) gameScreen.getScreens().get("end");
-        es.setWinner(2);
-        gameScreen.informUser("bal");
+        gameScreen.showCountDown();
 
 
         // configure and show stage
